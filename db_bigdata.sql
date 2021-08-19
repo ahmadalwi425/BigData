43bc68894d73b521e004467f0e2547ea54665c0a
ahmadalwi425@gmail.com <ahmadalwi425@gmail.com>
2021-08-19 13:51:45 +0700

    db

--- db_bigdata.sql
+++ db_bigdata.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 09, 2021 at 02:53 PM
+-- Generation Time: Aug 19, 2021 at 08:50 AM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.9
 
@@ -36,6 +36,16 @@ CREATE TABLE `buletin` (
   `id_kategori_buletin` bigint(20) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `buletin`
+--
+
+INSERT INTO `buletin` (`id`, `cover`, `judul`, `konten`, `id_kategori_buletin`) VALUES
+(1, 'img/IMG_6877.JPG', 'Bul1-kat1', 'tes', 1),
+(2, 'img/IMG_6877.JPG', 'Bul2-kat2', 'test2', 2),
+(3, 'img/IMG_6877.JPG', 'bul3', 'tes', 1),
+(4, 'img/IMG_6877.JPG', 'bul4', 'tes', 2);
+
 -- --------------------------------------------------------
 
 --
@@ -51,13 +61,48 @@ CREATE TABLE `daftar_ormawa` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `data_kampus`
+--
+
+CREATE TABLE `data_kampus` (
+  `id` int(11) NOT NULL,
+  `kategori` varchar(50) NOT NULL,
+  `total` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `data_kampus`
+--
+
+INSERT INTO `data_kampus` (`id`, `kategori`, `total`) VALUES
+(1, 'Fakultas', 10),
+(2, 'Program Studi', 15),
+(3, 'Dosen', 2),
+(4, 'Mahasiswa', 1000),
+(5, 'Himpunan Mahasiswa', 10),
+(6, 'Unit Kegiatan Mahasiswa (UKM)', 90),
+(7, 'Himpunan Daerah', 8);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `divisi`
 --
 
 CREATE TABLE `divisi` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `nama_divisi` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
+  `nama_divisi` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `deskripsi` text COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `divisi`
+--
+
+INSERT INTO `divisi` (`id`, `nama_divisi`, `deskripsi`) VALUES
+(1, 'BIGDATA ORGANISASI MAHASISWA', 'Bigdata organisasi mahasiswa merupakan data tentang organisasi mahasiswa di lingkungan Institut Teknologi PLN yang di peroleh dari Biro Kominfo BEM KBM IT-PLN'),
+(2, 'BIGDATA KEMENTRIAN BEM KBM IT-PLN', 'Bigdata Kemnetrian merupakan data tentang kementrian yang ada di BEM KBM IT-PLN yang diperoleh dari Biro Kominfo'),
+(3, 'BIGDATA BIRO BEM KBM IT-PLN', 'Bigdata Biro merupakan data tentang kebiroan yang ada di BEM KBM IT-PLN yang diperoleh dari Biro Kominfo');
 
 -- --------------------------------------------------------
 
@@ -86,6 +131,24 @@ CREATE TABLE `jenis_kal` (
   `nama_jenis_kal` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `jenis_kal`
+--
+
+INSERT INTO `jenis_kal` (`id`, `nama_jenis_kal`) VALUES
+(1, 'Hari Libur Nasional');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jenis_produk`
+--
+
+CREATE TABLE `jenis_produk` (
+  `id` int(11) NOT NULL,
+  `jenis_produk` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 -- --------------------------------------------------------
 
 --
@@ -97,6 +160,15 @@ CREATE TABLE `jurusan` (
   `nama_jurusan` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `jurusan`
+--
+
+INSERT INTO `jurusan` (`id`, `nama_jurusan`) VALUES
+(1, 'jurusan 1'),
+(2, 'jurusan 2'),
+(3, 'jurusan 3');
+
 -- --------------------------------------------------------
 
 --
@@ -105,12 +177,20 @@ CREATE TABLE `jurusan` (
 
 CREATE TABLE `kal_akademik` (
   `id` bigint(20) UNSIGNED NOT NULL,
+  `nama_kegiatan` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `tgl_mulai` date NOT NULL,
   `tgl_selesai` date NOT NULL,
   `id_jenis_kal` bigint(20) UNSIGNED NOT NULL,
   `id_tahun_ajar` bigint(20) UNSIGNED NOT NULL,
   `semester` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `kal_akademik`
+--
+
+INSERT INTO `kal_akademik` (`id`, `nama_kegiatan`, `tgl_mulai`, `tgl_selesai`, `id_jenis_kal`, `id_tahun_ajar`, `semester`) VALUES
+(1, 'Hari Raya Pancasila', '2021-07-01', '2021-07-03', 1, 1, 'Genap');
 
 -- --------------------------------------------------------
 
@@ -123,6 +203,14 @@ CREATE TABLE `kategori_buletin` (
   `nama_kategori` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `kategori_buletin`
+--
+
+INSERT INTO `kategori_buletin` (`id`, `nama_kategori`) VALUES
+(1, 'KAJIAN BEM KBM IT-PLN'),
+(2, 'INFOGRAFIS KBM IT-PLN');
+
 -- --------------------------------------------------------
 
 --
@@ -133,6 +221,14 @@ CREATE TABLE `level` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `nama_level` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `level`
+--
+
+INSERT INTO `level` (`id`, `nama_level`) VALUES
+(1, 'admin'),
+(2, 'Mahasiswa');
 
 -- --------------------------------------------------------
 
@@ -180,6 +276,13 @@ CREATE TABLE `ormawa` (
   `nama_ormawa` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `ormawa`
+--
+
+INSERT INTO `ormawa` (`id`, `id_jurusan`, `nama_ormawa`) VALUES
+(1, 1, 'ormawa 1');
+
 -- --------------------------------------------------------
 
 --
@@ -195,6 +298,23 @@ CREATE TABLE `password_resets` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `pembelian`
+--
+
+CREATE TABLE `pembelian` (
+  `id` int(11) NOT NULL,
+  `id_pembeli` bigint(20) UNSIGNED NOT NULL,
+  `id_produk` int(11) NOT NULL,
+  `total_harga` int(11) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `bukti` varchar(250) NOT NULL,
+  `tanggal` datetime NOT NULL,
+  `status` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `peminjaman`
 --
 
@@ -204,8 +324,24 @@ CREATE TABLE `peminjaman` (
   `tgl_pinjam` date NOT NULL,
   `tgl_kembali` date DEFAULT NULL,
   `barang` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `status` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL
+  `status` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'dipinjam'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produk`
+--
+
+CREATE TABLE `produk` (
+  `id` int(11) NOT NULL,
+  `id_jenis_produk` int(11) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `harga` bigint(20) NOT NULL,
+  `gambar` varchar(250) NOT NULL,
+  `id_penjual` bigint(20) UNSIGNED NOT NULL,
+  `rekening` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -218,6 +354,30 @@ CREATE TABLE `subdivisi` (
   `nama_subdivisi` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `id_divisi` bigint(20) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `subdivisi`
+--
+
+INSERT INTO `subdivisi` (`id`, `nama_subdivisi`, `id_divisi`) VALUES
+(1, 'Bigdata BEM KBM IT-PLN', 1),
+(2, 'Bigdata UKM', 1),
+(3, 'Bigdata Himpunan Mahasiswa', 1),
+(4, 'Bigdata Himpunan Daerah', 1),
+(5, 'ADKESMA', 2),
+(6, 'DEDMAS', 2),
+(7, 'EKOKRAF', 2),
+(8, 'HARMAWA', 2),
+(9, 'KASTRAT', 2),
+(10, 'AGAMA', 2),
+(11, 'PSDM', 2),
+(12, 'KPP', 2),
+(13, 'P&K', 2),
+(14, 'RISTEK', 2),
+(15, 'SENBUDORA', 2),
+(16, 'KESEKRETARIATAN', 3),
+(17, 'INVENTARIS', 3),
+(18, 'KEUANGAN', 3);
 
 -- --------------------------------------------------------
 
@@ -243,6 +403,13 @@ CREATE TABLE `tahun_ajar` (
   `tahun_ajar` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `tahun_ajar`
+--
+
+INSERT INTO `tahun_ajar` (`id`, `tahun_ajar`) VALUES
+(1, '2020/2021');
+
 -- --------------------------------------------------------
 
 --
@@ -265,6 +432,13 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `nim`, `nama`, `email`, `email_verified_at`, `password`, `no_hp`, `id_level`, `id_jurusan`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 123123, 'user1', 'user@user.com', NULL, '$2y$10$xJ1hv4KmzcyQXot4kUfZdOfhO4mRt60Tc7zZy3VEpJ4RLkY8Lw8Ae', 5674563, 2, 1, NULL, '2021-08-11 07:12:26', '2021-08-11 07:12:26');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -284,6 +458,12 @@ ALTER TABLE `daftar_ormawa`
   ADD KEY `id_ormawa` (`id_ormawa`);
 
 --
+-- Indexes for table `data_kampus`
+--
+ALTER TABLE `data_kampus`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `divisi`
 --
 ALTER TABLE `divisi`
@@ -300,6 +480,12 @@ ALTER TABLE `failed_jobs`
 -- Indexes for table `jenis_kal`
 --
 ALTER TABLE `jenis_kal`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `jenis_produk`
+--
+ALTER TABLE `jenis_produk`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -348,11 +534,27 @@ ALTER TABLE `password_resets`
   ADD KEY `password_resets_email_index` (`email`);
 
 --
+-- Indexes for table `pembelian`
+--
+ALTER TABLE `pembelian`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_pembeli` (`id_pembeli`),
+  ADD KEY `id_barang` (`id_produk`);
+
+--
 -- Indexes for table `peminjaman`
 --
 ALTER TABLE `peminjaman`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_user` (`id_user`);
+
+--
+-- Indexes for table `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_penjual` (`id_penjual`),
+  ADD KEY `id_jenis_produk` (`id_jenis_produk`);
 
 --
 -- Indexes for table `subdivisi`
@@ -392,7 +594,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `buletin`
 --
 ALTER TABLE `buletin`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `daftar_ormawa`
@@ -401,10 +603,16 @@ ALTER TABLE `daftar_ormawa`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `data_kampus`
+--
+ALTER TABLE `data_kampus`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
 -- AUTO_INCREMENT for table `divisi`
 --
 ALTER TABLE `divisi`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -416,31 +624,37 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `jenis_kal`
 --
 ALTER TABLE `jenis_kal`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `jenis_produk`
+--
+ALTER TABLE `jenis_produk`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `jurusan`
 --
 ALTER TABLE `jurusan`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `kal_akademik`
 --
 ALTER TABLE `kal_akademik`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `kategori_buletin`
 --
 ALTER TABLE `kategori_buletin`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `level`
 --
 ALTER TABLE `level`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `migrations`
@@ -452,7 +666,13 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `ormawa`
 --
 ALTER TABLE `ormawa`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `pembelian`
+--
+ALTER TABLE `pembelian`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `peminjaman`
@@ -461,10 +681,16 @@ ALTER TABLE `peminjaman`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `subdivisi`
 --
 ALTER TABLE `subdivisi`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `submenu`
@@ -476,13 +702,13 @@ ALTER TABLE `submenu`
 -- AUTO_INCREMENT for table `tahun_ajar`
 --
 ALTER TABLE `tahun_ajar`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
@@ -508,10 +734,30 @@ ALTER TABLE `kal_akademik`
   ADD CONSTRAINT `kal_akademik_ibfk_2` FOREIGN KEY (`id_tahun_ajar`) REFERENCES `tahun_ajar` (`id`) ON UPDATE CASCADE;
 
 --
+-- Constraints for table `ormawa`
+--
+ALTER TABLE `ormawa`
+  ADD CONSTRAINT `ormawa_ibfk_1` FOREIGN KEY (`id_jurusan`) REFERENCES `jurusan` (`id`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `pembelian`
+--
+ALTER TABLE `pembelian`
+  ADD CONSTRAINT `pembelian_ibfk_1` FOREIGN KEY (`id_produk`) REFERENCES `produk` (`id`),
+  ADD CONSTRAINT `pembelian_ibfk_2` FOREIGN KEY (`id_pembeli`) REFERENCES `users` (`id`);
+
+--
 -- Constraints for table `peminjaman`
 --
 ALTER TABLE `peminjaman`
   ADD CONSTRAINT `peminjaman_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `produk`
+--
+ALTER TABLE `produk`
+  ADD CONSTRAINT `produk_ibfk_1` FOREIGN KEY (`id_jenis_produk`) REFERENCES `jenis_produk` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `produk_ibfk_2` FOREIGN KEY (`id_penjual`) REFERENCES `users` (`id`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `subdivisi`
