130b01678556c97c73db2af7a836eeedf23d8e5c
ahmadalwi425@gmail.com <ahmadalwi425@gmail.com>
2021-08-21 11:38:10 +0700

    submenu

--- app/Http/Controllers/submenuController.php
+++ app/Http/Controllers/submenuController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\submenu;
+use App\Models\subdivisi;
 
 class submenuController extends Controller
 {
--- app/Models/submenu.php
+++ app/Models/submenu.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Moedls\subdivisi;
+use App\Models\subdivisi;
 
 class submenu extends Model
 {
