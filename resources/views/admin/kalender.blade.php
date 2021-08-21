662fc4491713bdf011a5b838ed2f021753af6b81
amarizzal <rizal.ammar91@yahoo.com>
2021-08-21 16:57:19 +0700

    update FE

--- resources/views/admin/kalender.blade.php
+++ resources/views/admin/kalender.blade.php
@@ -56,7 +56,6 @@
                   <th scope="col" class="sort" data-sort="name">Nama Kegiatan</th>
                   <th scope="col" class="sort" data-sort="name">Semester</th>
                   <th scope="col" class="sort" data-sort="name">Aksi</th>
-                  <th scope="col"></th>
                 </tr>
               </thead>
               <tbody class="list">
@@ -119,125 +118,132 @@
             </nav>
           </div>
         </div>
-        <div class="row col-md-12">
-          <div class="card col-md-6">
-          <!-- Card header -->
-          <div class="card-header border-0">
-            <div class="mb-0 pb-0 row justify-ceontent-center">
-              <h3 class="mb-0 col-8">Jenis Event</h3>
-              <button type="button" data-toggle="modal" data-target="#create" class="btn-success btn col-4 mb-0">Tambah</button>
+        <div class="row">
+          <div class="col-md-6 col-12">
+            <div class="card">
+              <!-- Card header -->
+              <div class="card-header border-0">
+                <div class="mb-0 pb-0 row justify-ceontent-center">
+                  <h3 class="mb-0 col-8">Jenis Event</h3>
+                  <button type="button" data-toggle="modal" data-target="#create" class="btn-success btn col-4 mb-0">Tambah</button>
+                </div>
+              </div>
+              <div class="card-body">
+                <!-- Light table -->
+                <div class="table-responsive">
+                  <table class="table align-items-center table-flush">
+                    <thead class="thead-light">
+                      <tr>
+                        <th scope="col" class="sort" data-sort="name">Nama Jenis</th>
+                        <th scope="col" class="sort" data-sort="name">Aksi</th>
+                      </tr>
+                    </thead>
+                    <tbody class="list">
+                      @foreach($jenis_kal as $row)
+                      <tr>
+                        <td>
+                            {{$row->nama_jenis_kal}}
+                        </td>
+                        <td>
+                            <button class="btn btn-warning" type="button"data-toggle="modal" data-target="#exampleModal">Detail</button>
+                        </td>
+                      </tr>
+                      @endforeach
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+              <!-- Card footer -->
+              <div class="card-footer py-4">
+                <nav aria-label="...">
+                  <ul class="pagination justify-content-end mb-0">
+                    <li class="page-item disabled">
+                      <a class="page-link" href="#" tabindex="-1">
+                        <i class="fas fa-angle-left"></i>
+                        <span class="sr-only">Previous</span>
+                      </a>
+                    </li>
+                    <li class="page-item active">
+                      <a class="page-link" href="#">1</a>
+                    </li>
+                    <li class="page-item">
+                      <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item">
+                      <a class="page-link" href="#">
+                        <i class="fas fa-angle-right"></i>
+                        <span class="sr-only">Next</span>
+                      </a>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
             </div>
           </div>
-          <!-- Light table -->
-          <div class="table-responsive">
-            <table class="table align-items-center table-flush">
-              <thead class="thead-light">
-                <tr>
-                  <th scope="col" class="sort" data-sort="name">Nama Jenis</th>
-                  <th scope="col" class="sort" data-sort="name">Aksi</th>
-                  <th scope="col"></th>
-                </tr>
-              </thead>
-              <tbody class="list">
-                @foreach($jenis_kal as $row)
-                <tr>
-                  <td>
-                      {{$row->nama_jenis_kal}}
-                  </td>
-                  <td>
-                      <button class="btn btn-warning" type="button"data-toggle="modal" data-target="#exampleModal">Detail</button>
-                  </td>
-                </tr>
-                @endforeach
-              </tbody>
-            </table>
-          </div>
-          <!-- Card footer -->
-          <div class="card-footer py-4">
-            <nav aria-label="...">
-              <ul class="pagination justify-content-end mb-0">
-                <li class="page-item disabled">
-                  <a class="page-link" href="#" tabindex="-1">
-                    <i class="fas fa-angle-left"></i>
-                    <span class="sr-only">Previous</span>
-                  </a>
-                </li>
-                <li class="page-item active">
-                  <a class="page-link" href="#">1</a>
-                </li>
-                <li class="page-item">
-                  <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                <li class="page-item">
-                  <a class="page-link" href="#">
-                    <i class="fas fa-angle-right"></i>
-                    <span class="sr-only">Next</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-        <div class="card col-md-6">
-          <!-- Card header -->
-          <div class="card-header border-0">
-            <div class="mb-0 pb-0 row justify-ceontent-center">
-              <h3 class="mb-0 col-8">Tahun Ajar</h3>
-              <button type="button" data-toggle="modal" data-target="#create" class="btn-success btn col-4 mb-0">Tambah</button>
+          <div class="col-md-6 col-12">
+            <div class="card">
+              <!-- Card header -->
+              <div class="card-header border-0">
+                <div class="mb-0 pb-0 row justify-ceontent-center">
+                  <h3 class="mb-0 col-8">Tahun Ajar</h3>
+                  <button type="button" data-toggle="modal" data-target="#create" class="btn-success btn col-4 mb-0">Tambah</button>
+                </div>
+              </div>
+              <div class="card-body">
+                <!-- Light table -->
+                <div class="table-responsive">
+                  <table class="table align-items-center table-flush">
+                    <thead class="thead-light">
+                      <tr>
+                        <th scope="col" class="sort col-md-6" data-sort="name">Tahun Ajar</th>
+                        <th scope="col" class="sort col-md-6" data-sort="name">Aksi</th>
+                        <th scope="col"></th>
+                      </tr>
+                    </thead>
+                    <tbody class="list">
+                      @foreach($tahun_ajar as $row)
+                      <tr>
+                        <td class="col-md-6">
+                            {{$row->tahun_ajar}}
+                        </td>
+                        <td  class="col-md-6">
+                            <button class="btn btn-warning" type="button"data-toggle="modal" data-target="#exampleModal">Detail</button>
+                        </td>
+                      </tr>
+                      @endforeach
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+              <!-- Card footer -->
+              <div class="card-footer py-4">
+                <nav aria-label="...">
+                  <ul class="pagination justify-content-end mb-0">
+                    <li class="page-item disabled">
+                      <a class="page-link" href="#" tabindex="-1">
+                        <i class="fas fa-angle-left"></i>
+                        <span class="sr-only">Previous</span>
+                      </a>
+                    </li>
+                    <li class="page-item active">
+                      <a class="page-link" href="#">1</a>
+                    </li>
+                    <li class="page-item">
+                      <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item">
+                      <a class="page-link" href="#">
+                        <i class="fas fa-angle-right"></i>
+                        <span class="sr-only">Next</span>
+                      </a>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
             </div>
           </div>
-          <!-- Light table -->
-          <div class="table-responsive">
-            <table class="table align-items-center table-flush">
-              <thead class="thead-light">
-                <tr>
-                  <th scope="col" class="sort col-md-6" data-sort="name">Tahun Ajar</th>
-                  <th scope="col" class="sort col-md-6" data-sort="name">Aksi</th>
-                  <th scope="col"></th>
-                </tr>
-              </thead>
-              <tbody class="list">
-                @foreach($tahun_ajar as $row)
-                <tr>
-                  <td class="col-md-6">
-                      {{$row->tahun_ajar}}
-                  </td>
-                  <td  class="col-md-6">
-                      <button class="btn btn-warning" type="button"data-toggle="modal" data-target="#exampleModal">Detail</button>
-                  </td>
-                </tr>
-                @endforeach
-              </tbody>
-            </table>
-          </div>
-          <!-- Card footer -->
-          <div class="card-footer py-4">
-            <nav aria-label="...">
-              <ul class="pagination justify-content-end mb-0">
-                <li class="page-item disabled">
-                  <a class="page-link" href="#" tabindex="-1">
-                    <i class="fas fa-angle-left"></i>
-                    <span class="sr-only">Previous</span>
-                  </a>
-                </li>
-                <li class="page-item active">
-                  <a class="page-link" href="#">1</a>
-                </li>
-                <li class="page-item">
-                  <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                <li class="page-item">
-                  <a class="page-link" href="#">
-                    <i class="fas fa-angle-right"></i>
-                    <span class="sr-only">Next</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
         </div>
         
       </div>
