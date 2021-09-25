61c3825c6087f34c96153b84a5ece87f7b825b66
amarizzal <rizal.ammar91@yahoo.com>
2021-09-25 20:42:30 +0700

    pull

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,30 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <title>Cart | E-Shopper</title>
-    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
-    <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet">
-    <link href="{{asset('css/prettyPhoto.cs')}}s" rel="stylesheet">
-    <link href="{{asset('css/price-range.css')}}" rel="stylesheet">
-    <link href="{{asset('css/animate.css')}}" rel="stylesheet">
-	<link href="{{asset('css/main.css')}}" rel="stylesheet">
-	<link href="{{asset('css/responsive.css')}}" rel="stylesheet">
-    <!--[if lt IE 9]>
-    <script src="js/html5shiv.js"></script>
-    <script src="js/respond.min.js"></script>
-    <![endif]-->       
-    <link rel="shortcut icon" href="images/ico/favicon.ico">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
-</head><!--/head-->
+@extends('layouts.templateShop')
 
-<body>
+@section('content')
+
 	<header id="header"><!--header-->
 		<div class="header_top"><!--header_top-->
 			<div class="container">
@@ -434,14 +411,4 @@
 		
 	</footer><!--/Footer-->
 	
-
-
-    <script src="{{asset('js/jquery.js')}}"></script>
-	<script src="{{asset('js/price-range.js')}}"></script>
-    <script src="{{asset('js/jquery.scrollUp.min.js')}}"></script>
-	<script src="{{asset('js/bootstrap.min.js')}}"></script>
-    <script src="{{asset('js/jquery.prettyPhoto.js')}}"></script>
-    <script src="{{asset('js/main.js')}}"></script>
-</body>
-</body>
-</html>
+	@endsection
---
+++ resources/views/layouts/templateShop.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Cart | E-Shopper</title>
+    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet">
+    <link href="{{asset('css/prettyPhoto.cs')}}s" rel="stylesheet">
+    <link href="{{asset('css/price-range.css')}}" rel="stylesheet">
+    <link href="{{asset('css/animate.css')}}" rel="stylesheet">
+	<link href="{{asset('css/main.css')}}" rel="stylesheet">
+	<link href="{{asset('css/responsive.css')}}" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->       
+    <link rel="shortcut icon" href="images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
+	{{-- Sweetalert --}}
+	<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</head><!--/head-->
+
+<body>
+
+    <main class="main-content" id="panel">
+        @yield('content')
+    </main>
+
+@if(Session::has('success'))
+    <script>
+        Swal.fire(
+          'Berhasil!',
+          '{{session()->get('success')}}',
+          'success'
+        )
+    </script>
+  	@endif
+
+    <script src="{{asset('js/jquery.js')}}"></script>
+	<script src="{{asset('js/price-range.js')}}"></script>
+    <script src="{{asset('js/jquery.scrollUp.min.js')}}"></script>
+	<script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('js/jquery.prettyPhoto.js')}}"></script>
+    <script src="{{asset('js/main.js')}}"></script>
+</body>
+</body>
+</html>
