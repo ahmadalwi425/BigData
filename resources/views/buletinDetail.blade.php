ffe49b0ab7caaf55b04cff780dc8c5c7854d4625
amarizzal <rizal.ammar91@yahoo.com>
2021-10-05 16:09:37 +0700

    buletin detail

---
+++ resources/views/buletinDetail.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.template')
+
+@section('content')
+
+<div class="container -mt-5">
+    <h1 class="title title-buletin">
+
+    </h1>
+    <p class="content-buletin">
+        
+    </p>
+</div>
+
+
+@endsection
